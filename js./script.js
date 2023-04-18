24df2cdff6b0acfab7413f2e82558752a8477518
Tyler Wherry <120048745+WherrysTyler@users.noreply.github.com>
2023-04-18 12:35:29 -0700

    intro

--- js./script.js
+++ js./script.js
@@ -2,25 +2,37 @@
   const bg = document.querySelector('.bg')
    const intro1 = document.querySelector('.intro')
 
-  let load = 0
+  let loadNumber = 0
+  let loadIntro = 0
 
   let int = setInterval(blurring, 100)
 
   function blurring() {
 
-    load++
+    loadNumber++
 
-    if (load > 99) {
+    if (loadNumber > 99) {
     clearInterval(int)
+    introBlur()
+  }
+}
+  function introBlur() {
+
+    loadIntro++
+
+    if (loadIntro > 100) {
+      clearInterval(int)
+    }
   }
 
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  intro1.style.opacity = scale(load, 100, 0, 1, 0);
+
+  loadText.innerText = `${loadNumber}%`
+  loadText.style.opacity = scale(loadNumber, 0, 100, 1, 0);
+  intro1.style.opacity = scale(loadIntro, 100, 0, 1, 0);
   // intro1.innerText = `${load}%`
-  intro1.style.filter = `blur(${scale(load, 0, 100, 20, 0)})`
-  bg.style.filter = `blur(${scale(load, 0, 100, 20, 0)}px)`
-}
+  intro1.style.filter = `blur(${scale(loadIntro, 0, 100, 20, 0)})`
+  bg.style.filter = `blur(${scale(loadNumber, 0, 100, 20, 0)}px)`
+
 
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
